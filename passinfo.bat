9afc8e6cc2ad4620ec279347fa0caad07685f3e9
morphouslord <70637311+morphouslord@users.noreply.github.com>
2020-09-16 14:27:27 +0530

    Add files via upload

---
+++ passinfo.bat
@@ -0,0 +1,287 @@
+@echo off 
+mode 2000
+color 04
+echo server adderss. 
+echo u can run a ping with google.
+echo rout multiple network.
+echo ur all password is transfered to ur file manager u can get it by following the steps.
+echo u should do this after launching the process.
+echo go to the file c:\ directory.
+echo open the file. 
+echo go to the discription (xml file ).
+echo search for key meterial and that's ur password. 
+echo note ur all password is loaded in it whic is secured in ur pc.
+echo this even includes ur ip.
+echo u will get an hardware audit and a overall analysis of ur computer.
+echo =======================================================================================
+echo lets start the process.
+echo =======================================================================================
+echo """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+echo "                             10000001101(1  11.  1/110001.                                          "     
+echo "                          1001                     .1. 1..11. .101                                                   "      
+echo "                      101                                    .. 1( (0/                         "     
+echo "                   001                                         1 .1.11 /01                                     "    
+echo "                01                                               . 11 .1. 101                                        "   
+echo "             01 .                                        ..           .11 .1 001                                 "  
+echo "          0(1/                                                1          1  11/01                       "
+echo "         01.                                                        1.       ..1 10              "
+echo "       001                                                       .      /      11111/              "
+echo "     .0 1                                                           1      .1     .110                       "
+echo "    11 (                                                               1.     .11  1.10(                         "
+echo "    0.1                                                                   ..     .1 11 00.                 "
+echo "   01/  1.                                                              1          (/. .11              "
+echo "  1(/  1..1                                                               1       0. . /10               "
+echo "  01. . (1 (                                                            ..  .    011/ .. 0/                  "
+echo " 10111 /..(1       0                                                     /..    /1/(1. . 11                                   "
+echo "  011(101110      1                                                          .  1//1111  /1               "
+echo "  11.10(/010     1                                                        .     1/1//1 ( /1                                          "
+echo "  01 /  1/.1    1     11             .        (  .     1             1(     1  1 0/11.1. /0                                    "
+echo "  0/ (1(1/1/  /  .              .       .  1  0/11                         1  11.0//11 1.(/                   "
+echo "  /1 /1.1/0.1//      (0001 ((1/ ( 000. 11/1 .1/1  1  . 10((.1 (/((10100/      1/1(11(   10               "
+echo "   0    .100     011/1(111./1((/111./1011.(110 (1.1 1.01/11  1(1..1 0/11110/     00...  11          "
+echo "   11 /111     1/1((1111111..1/11 (/ 1(1..0      (./1101.1 .1/..(10 11/1110/01   100/.. 0                          "
+echo "    01 100    1(/(/ ((1/101/010(((/1( 0/..11  .  0(1/01/1.11111/0111(1/01/(1(01  ((0((/11                              "
+echo "     0 .0     1((/(11(11((1(0001001 /01   0..    (01110001010/010(11//10(/((1(0  1 /01 0                                     "
+echo "     .00  1   10111(/ 1(/.00000010100/  1.010(010(01(/1100100/101111100101 110 1  .11001                                               "
+echo "  1001    .0   10// .1(1(111100/100.1 1   001/01((01 1011100.1011 0(1(11(1110(   1.. 1.0001                             "
+echo "0(           0.  000(11/((010001   1.  1 0(000001110.11(1    /00011.0(0.100    1  . 1   1  /0                                                    "
+echo " 01            10(/  .                . 00001001011(011 /(1 .             . 0.1 . . .111110/                                       "
+echo "    011... .1.  ..1 ..111   (1         00(0(10010011001(11 11 .1.    ..  1 .1 111/   1/0(                                "
+echo "       00000100000  1.1 1  1(.        01001111101100001111     1/    111 1 (11/1101(00                                  "
+echo "        0/./..  0(00.111      1    1  010001(00001000/1  1.   . .  1 /1110((0 10.1/0                      "
+echo "         (0./1  1.  00.11    1     1  .0010001 1001001...111  . .1./.(/0    0  /1(0                                       "
+echo "          .00   1(   (0/1 1/..      1000.1(      .11/000 . 1  1 1. 1(10    11/.101                            "
+echo "           10.1 00    101  (           .            11    .1 .1 1.1/.1     0(11(0                                        "
+echo "            0/  /01    10011.//  /   .                1   .1 1.1/ 10/1    10./.0/                                                                     "
+echo "            0    000   0(/1..(1 ..  .    1             .   .  11  1 10   100 .//.                                               "
+echo "            //   1001  0001((0/1.1(/10(10((10101/.101 1/(  1. .//011(0 1010  .10.                                       "
+echo "            101.  1(1110(0 ( 1.  0  /.   1   101   1   1   01.1(.0(010(010.  110                              "
+echo "            .0.1   .0(101( (000/ 11 01  (.   10    /   0 .111111../1(0/(1   1 /0                                 "
+echo "             0.1     01101 1  /(  0  /1  11110111..1.111 111.1.11(.10111   .1110                                             "
+echo "             0/. .           100.  0 .1  0.  10   1/  1  0 10101    .       1/10                                          "
+echo "             0(1..    1    111  1  10001000000000100001011  .    111  1   . 110                                                                    "
+echo "              011..1   1   . 111   /1.  . 1   1      1  1    .  1    ..(.   ./0                                                              "
+echo "              1/1.11    .    ..1     .11..11(11 1.111..1 /11. .1/      . 1...10                                            "
+echo "                01(1 1.  (         ..1/(1/(1110//.( /. 1 . 1 ..    1  .1 . .0.                                    "
+echo "                  101.                                 (1/0(   .  .     101                                           "
+echo "                      001..                            . 111... . . 100.                                                      "
+echo "                         01 .               1.         .    11 1(/10                                               "
+echo "                            10. 1                     1/1. .1 100                                               "
+echo "                               00/1.         .1       ..111001                                                              "
+echo "                                     .111/1       11111                                                      "
+echo """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""" 
+ECHO "101010HHHHHHHH        HHHHHHHH 01010101010AAAAAAAAAAAAAAAA 1010101010CCCCCCCCCCCCCCCCCCCCCCCCCC 0KKKKKK           KKKKKK  "
+ECHO "110011HHHHHHHH        HHHHHHHH 1010101001AAAAAAAA  AAAAAAAA 101010101CCCCCCCCCCCCCCCCCCCCCCCCCC 1KKKKKK          KKKKKK "
+ECHO "001100HHHHHHHH        HHHHHHHH 010101010AAAAAAAA    AAAAAAAA 10101010CCCCCCC            CCCCCCC 0KKKKKK         KKKKKK "
+ECHO "101010HHHHHHHH        HHHHHHHH 10101010AAAAAAAA      AAAAAAAA 0101010CCCCCCC                    1KKKKKK        KKKKKK "
+ECHO "010101HHHHHHHHHHHHHHHHHHHHHHHH 0101010AAAAAAAA        AAAAAAAA 101010CCCCCCC                    0KKKKKK       KKKKKK "
+ECHO "011101HHHHHHHHHHHHHHHHHHHHHHHH 101010AAAAAAAAAAAAAAAAAAAAAAAAAA 10101CCCCCCC                    1KKKKKKKKKKKKKKKKKKK "
+ECHO "100011HHHHHHHHHHHHHHHHHHHHHHHH 01010AAAAAAAAAAAAAAAAAAAAAAAAAAAA 1010CCCCCCC                    0KKKKKKKKKKKKKKKKKKK " 
+ECHO "110001HHHHHHHH        HHHHHHHH 1010AAAAAAAA              AAAAAAAA 101CCCCCCC                    1KKKKKK       KKKKKK  " 
+ECHO "010100HHHHHHHH        HHHHHHHH 010AAAAAAAA                AAAAAAAA 10CCCCCCC                    0KKKKKK        KKKKKK "
+ECHO "101010HHHHHHHH        HHHHHHHH 10AAAAAAAA                  AAAAAAAA 0CCCCCCC            CCCCCCC 1KKKKKK         KKKKKK "
+ECHO "101010HHHHHHHH        HHHHHHHH 0AAAAAAAA                    AAAAAAAA0CCCCCCCCCCCCCCCCCCCCCCCCCC 0KKKKKK          KKKKKK "
+ECHO "101010HHHHHHHH        HHHHHHHH AAAAAAAA                      AAAAAAAACCCCCCCCCCCCCCCCCCCCCCCCCC 1KKKKKK           KKKKKK  BY-MORPHOUSLORD"  
+
+:menu
+echo  """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+echo  "                                                                          1) netstat                                                                     "
+echo  "                                                                          2) nbtstat                                                                     "
+echo  "                                                                          3) ipconfig/all                                                                "
+echo  "                                                                          4) ipconfig/displaydns                                                         "
+echo  "                                                                          5) wifi export                                                                 "
+echo  "                                                                          6) system info                                                                 "
+echo  "                                                                          7) tracert                                                                     "
+echo  "                                                                          8) ping                                                                        "
+echo  "                                                                          9) system transfer                                                             "
+echo  "                                                                          10) help                                                                       "                            
+echo  """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""" 
+set /p ans="enter nember : "
+
+if %ans% == 1 goto a
+
+if %ans% == 2 goto b
+
+if %ans% == 3 goto c
+
+if %ans% == 4 goto d
+
+if %ans% == 5 goto e
+
+if %ans% == 6 goto f
+
+if %ans% == 7 goto g
+
+if %ans% == 8 goto h
+
+if %ans% == 9 goto WINNT
+
+if %ans% == 10 goto i
+
+:a
+COLOR 5
+netstat -a -s
+pause
+goto menu 
+
+:b
+COLOR 4
+nbtstat -n
+pause
+goto menu
+
+:c
+COLOR 3
+ipconfig/all
+pause
+goto menue
+
+:d
+COLOR 6
+ipconfig/displaydns
+pause
+goto menue
+
+:e
+COLOR 9
+netsh wlan export profile folder=. key=clear
+pause
+goto menue
+
+:f
+COLOR 10
+systeminfo
+pause
+goto menue
+
+:g
+COLOR 02
+tracert www.google.com
+pause
+goto menue
+
+:h
+COLOR 22
+ping www.google.com -l 100 -n 10 -a
+pause
+goto menue
+
+:i
+COLOR 14
+echo * for netstat and  nbtstat u need to type ctrl c
+echo * system info exports all data info of system
+echo * wifi export exports xml files containing all the wifi password secured in ur system
+echo * system transfer transfer audit and a txt document to folder
+echo * trecent does a normal wifi check and nework check u can stop it by quiting the programes or just do ctrl c
+echo * ping does a normal ping to see the time duration taken the network
+echo * u can close the program if u end up in errors
+pause
+goto menue
+
+echo =======================================================================================
+echo process completed
+echo =======================================================================================
+if %os%==Windows_NT goto WINNT
+goto NOCON
+
+:WINNT
+COLOR 2
+echo .Using a Windows NT based system
+echo ..%computername%
+
+REM set variables
+set system=
+set manufacturer=
+set model=
+set serialnumber=
+set osname=
+set sp=
+setlocal ENABLEDELAYEDEXPANSION
+set "volume=C:"
+set totalMem=
+set availableMem=
+set usedMem=
+set IPv4=
+set Domain=
+
+echo Getting data [Computer: %computername%]...
+echo Please Wait....
+
+REM Get Computer Name
+FOR /F "tokens=2 delims='='" %%A in ('wmic OS Get csname /value') do SET system=%%A
+
+REM Get Computer Manufacturer
+FOR /F "tokens=2 delims='='" %%A in ('wmic ComputerSystem Get Manufacturer /value') do SET manufacturer=%%A
+
+REM Get Computer Model
+FOR /F "tokens=2 delims='='" %%A in ('wmic ComputerSystem Get Model /value') do SET model=%%A
+
+REM Get Computer Serial Number
+FOR /F "tokens=2 delims='='" %%A in ('wmic Bios Get SerialNumber /value') do SET serialnumber=%%A
+
+REM Get Computer OS
+FOR /F "tokens=2 delims='='" %%A in ('wmic os get Name /value') do SET osname=%%A
+FOR /F "tokens=1 delims='|'" %%A in ("%osname%") do SET osname=%%A
+
+REM Get Computer OS SP
+FOR /F "tokens=2 delims='='" %%A in ('wmic os get ServicePackMajorVersion /value') do SET sp=%%A
+
+REM Get Memory
+FOR /F "tokens=4" %%a in ('systeminfo ^| findstr Physical') do if defined totalMem (set availableMem=%%a) else (set totalMem=%%a)
+set totalMem=%totalMem:,=%
+set availableMem=%availableMem:,=%
+set /a usedMem=totalMem-availableMem
+
+FOR /f "tokens=1*delims=:" %%i IN ('fsutil volume diskfree %volume%') DO (
+    SET "diskfree=!disktotal!"
+    SET "disktotal=!diskavail!"
+    SET "diskavail=%%j"
+)
+FOR /f "tokens=1,2" %%i IN ("%disktotal% %diskavail%") DO SET "disktotal=%%i"& SET "diskavail=%%j"
+
+
+echo done!
+
+echo --------------------------------------------
+echo System Name: %system%
+echo Manufacturer: %manufacturer%
+echo Model: %model%
+echo Serial Number: %serialnumber%
+echo Operating System: %osname%
+echo C:\ Total: %disktotal:~0,-9% GB
+echo C:\ Avail: %diskavail:~0,-9% GB
+echo Total Memory: %totalMem%
+echo Used  Memory: %usedMem%
+echo Computer Processor: %processor_architecture%
+echo Service Pack: %sp%
+echo --------------------------------------------
+
+REM Generate file
+SET file="%~dp0%computername%.txt"
+echo -------------------------------------------- >> %file%
+echo Details For: %system% >> %file%
+echo Manufacturer: %manufacturer% >> %file%
+echo Model: %model% >> %file%
+echo Serial Number: %serialnumber% >> %file%
+echo Operating System: %osname% >> %file%
+echo C:\ Total: %disktotal:~0,-9% GB >> %file%
+echo C:\ Avail: %diskavail:~0,-9% GB >> %file%
+echo Total Memory: %totalMem% >> %file%
+echo Used  Memory: %usedMem% >> %file%
+echo Computer Processor: %processor_architecture% >> %file%
+echo Service Pack: %sp% >> %file%
+echo -------------------------------------------- >> %file%
+
+echo %system%, %model%, %serialnumber%, %osname%,%totalMem%,%usedMem%,%manufacturer%,%sp%,%disktotal:~0,-9%,%usedMem% > hardware-audit.csv
+REM systeminfo | findstr /c:"Host Name" /c:"OS Name" /c:"OS Version" /c:"System Manufacturer" /c:"System Model" /c:"System type" /c:"Total Physical Memory" /c:"Domain"
+
+REM request user to push any key to continue
+pause
+goto END
+:NOCON
+echo ("fuck u")
+echo Error...Invalid Operating System...
+echo Error...No actions were made...
+goto 
+-END
+
