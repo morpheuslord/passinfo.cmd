e8c09509143adf3fe59174bdb2f70fada2971223
morphouslord <70637311+morphouslord@users.noreply.github.com>
2020-09-29 12:42:36 +0530

    Update passinfo.bat

--- passinfo.bat
+++ passinfo.bat
@@ -1,18 +1,6 @@
 @echo off 
 mode 2000
 color 04
-echo server adderss. 
-echo u can run a ping with google.
-echo rout multiple network.
-echo ur all password is transfered to ur file manager u can get it by following the steps.
-echo u should do this after launching the process.
-echo go to the file c:\ directory.
-echo open the file. 
-echo go to the discription (xml file ).
-echo search for key meterial and that's ur password. 
-echo note ur all password is loaded in it whic is secured in ur pc.
-echo this even includes ur ip.
-echo u will get an hardware audit and a overall analysis of ur computer.
 echo =======================================================================================
 echo lets start the process.
 echo =======================================================================================
